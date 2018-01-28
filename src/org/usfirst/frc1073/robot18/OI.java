6919c5dd22a6a0e0499bb8801dbec7420044fb34
bugeybot <bugeybot@gmail.com>
2018-01-28 11:26:52 -0500

    Cube changes and Driver button additions
    added a cancel button and driver controller buttons

--- src/org/usfirst/frc1073/robot18/OI.java
+++ src/org/usfirst/frc1073/robot18/OI.java
@@ -3,6 +3,8 @@ package org.usfirst.frc1073.robot18;
 
 import org.usfirst.frc1073.robot18.XboxController;
 import org.usfirst.frc1073.robot18.commands.*;
+
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
@@ -11,37 +13,27 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
 	
 	public XboxController driverControl;
+	public JoystickButton RobotTeleInit;
+	public JoystickButton visionButton;
+	public JoystickButton lidarButton;
+	public JoystickButton cancel;
+	public boolean cancelPushed;
 	
     public OI() {
+    	
     	driverControl = new XboxController(0);
+    	
+    	RobotTeleInit = driverControl.start;
+    	
+    	visionButton = driverControl.b;
+    	visionButton.whenPressed(new VisionCubeTracker());
+    	
+    	lidarButton = driverControl.y;
+    	lidarButton.whenPressed(new LidarAlign());
+    	
+    	cancel = driverControl.a;
 
         // SmartDashboard Buttons
         SmartDashboard.putData("Drive", new ControllerDifferentialDrive());
