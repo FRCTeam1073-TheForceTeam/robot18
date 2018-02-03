fce65f082652c19538be4b68206baac3fcfc1b3d
Jack M <jackmcevoy15@gmail.com>
2018-02-03 16:06:37 -0500

    AutoSystem work + GearShift blank + FMS
    Trying to flesh out the AutoSystem with actual code but don't have any
    of the distances, will put distances in when I have the time to look
    over the blueprints.
    GearShift is empty and should be filled in order for drive team to get
    what they want out of autonomous.
    The results from FMS now appear in Robot as variable gameData as a
    string. Also there are individual side string variables for the switch
    and scale, if we want to put them on the driver station or something
    like that.

--- src/org/usfirst/frc1073/robot18/Robot.java
+++ src/org/usfirst/frc1073/robot18/Robot.java
@@ -2,6 +2,7 @@
 package org.usfirst.frc1073.robot18;
 
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -34,7 +35,7 @@ public class Robot extends IterativeRobot {
 	public static robotDrivetrain drivetrain;
 	public static CameraServer cameraSwitcher;
 	public static boolean selectedCamera;
-
+	public static String gameData;
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -168,6 +169,20 @@ public class Robot extends IterativeRobot {
 
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
+		String switchSide;
+		String scaleSide;
+		//gameData = DriverStation.getInstance().getGameSpecificMessage();
+		gameData = "RRR";
+		if(gameData.charAt(0) == 'L') {
+			switchSide = "left";
+		}else {
+			switchSide = "right";
+		}
+		if(gameData.charAt(1) == 'L') {
+			scaleSide = "left";
+		}else {
+			scaleSide = "right";
+		}
 	}
 
 	public void autonomousInit() {
--- src/org/usfirst/frc1073/robot18/commands/AutoSystem.java
+++ src/org/usfirst/frc1073/robot18/commands/AutoSystem.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc1073.robot18.commands;
 
+import org.usfirst.frc1073.robot18.Robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -8,151 +12,182 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoSystem extends CommandGroup {
 
     public AutoSystem() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    /*switch(position) {
-    case 1:
+    	
+    /**Selects an autonomous using choices from the AutoTable on the Smart Dashboard
+     * @author Jack
+     * @category Autonomous
+     * @param Position "1" to "3", left to right, in front of the driver stations
+     * @param Elevator Working "yes" or "no", if we can use the elevator to pick up cubes or not
+     * @param Objective Positions "LLL", "RRR", "LRL", "RLR", set by the FMS
+     * @param Others Scale "yes" or "no", whether other robots on the alliance can put their cubes on the scale
+     */
+    String position = null;
+    String elevatorWorking = null;
+    String othersScale = null;
+    if(DriverStation.Alliance.Blue != null) {
+    	String alliance = "blue";
+    }else {
+    	String alliance = "red";
+    }
+    /*
+    switch(position) {
+    case "1":
     	switch(elevatorWorking) {
-    	case yes:
-    		switch(objectivePositions) {
-    		case RRR:
+    	case "yes":
+    		switch(Robot.gameData) {
+    		case "RRR":
+    			addSequential(new DriveWithPID(1));
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise"));
+    			addSequential(new DriveWithPID(1));
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise")); //Turns and drives into position 3
+    			addSequential(new DriveWithPID(1)); //Drives forward to switch
+    			addSequential(new GearShift());
+    			//addSequential(new robotElevator.drop()); //Drops cube into switch
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise"));
+    			addSequential(new DriveWithPID(1));
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise"));
+    			addSequential(new DriveWithPID(1));
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise"));
+    			addSequential(new DriveWithPID(1)); //Moved to cube near platform zone
+    			//addSequential(new robotElevator.pickUp();
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise"));
+    			addSequential(new DriveWithPID(1)); //Move to scale
+    			for(int i = 0; i < 6 && !; i++ ) {
+    				//addSequential(new robotElevator.drop());
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise"));
+    			addSequential(new DriveWithPID(1)); //Turn around and drive to cube
+    			//addSequential(new robotElevator.pickUp();
+    			addSequential(new TurnWithGyro(.1, 90, "clockwise"));
+    			addSequential(new DriveWithPID(1));
+    			//addSequential(new robotElevator.drop());
+    			}
+    			break;
+    		case "LRL":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LRL:
+    		case "RLR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case RLR:
-    			addSequential(new DriveWithPID(1));
-    			break;
-    		case LLL:
+    		case "LLL":
     			switch(othersScale) {
-    			case yes:
+    			case "yes":
         			addSequential(new DriveWithPID(1));
         			break;
-    			case no:
+    			case "no":
         			addSequential(new DriveWithPID(1));
         			break;
     			}
     			break;
     		}
     			break;
-    	case no:
-    		switch(objectivePositions) {
-    		case RRR:
+    	case "no":
+    		switch(Robot.gameData) {
+    		case "RRR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LRL:
+    		case "LRL":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case RLR:
+    		case "RLR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LLL:
+    		case "LLL":
     			addSequential(new DriveWithPID(1));
     			break;
     		}
     			break;
     }
     			break;
-    case 2:
+    case "2":
     	switch(elevatorWorking) {
-    	case yes:
-    		switch(objectivePositions) {
-    		case RRR:
+    	case "yes":
+    		switch(Robot.gameData) {
+    		case "RRR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LRL:
+    		case "LRL":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case RLR:
+    		case "RLR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LLL:
+    		case "LLL":
     			switch(othersScale) {
-    			case yes:
+    			case "yes":
         			addSequential(new DriveWithPID(1));
         			break;
-    			case no:
+    			case "no":
         			addSequential(new DriveWithPID(1));
         			break;
     			}
     			break;
     		}
     			break;
-    	case no:
-    		switch(objectivePositions) {
-    		case RRR:
+    	case "no":
+    		switch(Robot.gameData) {
+    		case "RRR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LRL:
+    		case "LRL":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case RLR:
+    		case "RLR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LLL:
+    		case "LLL":
     			addSequential(new DriveWithPID(1));
     			break;
     		}
     			break;
     }
     			break;
-    case 3:
+    case "3":
     	switch(elevatorWorking) {
-    	case yes:
-    		switch(objectivePositions) {
-    		case RRR:
+    	case "yes":
+    		switch(Robot.gameData) {
+    		case "RRR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LRL:
+    		case "LRL":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case RLR:
+    		case "RLR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LLL:
+    		case "LLL":
     			switch(othersScale) {
-    			case yes:
+    			case "yes":
         			addSequential(new DriveWithPID(1));
         			break;
-    			case no:
+    			case "no":
         			addSequential(new DriveWithPID(1));
         			break;
     			}
     			break;
     		}
     			break;
-    	case no:
-    		switch(objectivePositions) {
-    		case RRR:
+    	case "no":
+    		switch(Robot.gameData) {
+    		case "RRR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LRL:
+    		case "LRL":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case RLR:
+    		case "RLR":
     			addSequential(new DriveWithPID(1));
     			break;
-    		case LLL:
+    		case "LLL":
     			addSequential(new DriveWithPID(1));
     			break;
     		}
     			break;
     }
     			break;
-    */
+    
     }}
+
+	private void until(boolean b) {
+		// TODO Auto-generated method stub
+	*/	
+	}}
