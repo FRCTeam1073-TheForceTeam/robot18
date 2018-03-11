3d402a08b2f9d4195779c439f2c4e5e839dea03d
Jack M <jackmcevoy15@gmail.com>
2018-03-11 14:51:32 -0400

    Center To Scale Auto
    C_LL auto made
    Elevator changes will be removed in next push when this is combined with lidar

--- src/org/usfirst/frc1073/robot18/commands/Auto3CommandC_LL.java
+++ src/org/usfirst/frc1073/robot18/commands/Auto3CommandC_LL.java
@@ -6,10 +6,18 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Auto3CommandC_LL extends CommandGroup {
 	/** If Chooser is set to Center and FMS is LLL */
 	public Auto3CommandC_LL(){
-		addSequential(new AdvancedDrive(-.8, 10, 20));
+		/*addSequential(new AdvancedDrive(-.8, 10, 20));
 		addSequential(new TurnWithGyro(.65, 90, "counterclockwise"));
 		addSequential(new Dropoff(.5, "right")); //Drops cube onto floor near exchange
 		addSequential(new TurnWithGyro(.65, 90, "counterclockwise"));
-		addSequential(new CubeGetter()); //Should cross auto line on way to grab cube
+		addSequential(new CubeGetter()); //Should cross auto line on way to grab cube*/
+		addSequential(new AdvancedDrive(-.8, 6, 0));
+		addSequential(new TurnWithGyro(.8, 45, "counterclockwise"));
+		addSequential(new AdvancedDrive(-.8, 160, 0));
+		addSequential(new TurnWithGyro(.8, 45, "clockwise"));
+		addParallel(new LiftElevatorToDistanceScale(30));
+		addSequential(new AdvancedDrive(-.8, 160, 0));
+		addSequential(new TurnWithGyro(.6, 90, "counterclockwise"));
+		addSequential(new SpitOutCube(1, 1));
 	}
 }
