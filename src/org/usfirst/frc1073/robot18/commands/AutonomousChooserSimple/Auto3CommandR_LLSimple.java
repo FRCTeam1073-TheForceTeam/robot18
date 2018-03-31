aae4583723727c0ef80bc1ffe5ae149ef8b22a7e
Zeker10n <tristan.edward.hoenninger@gmail.com>
2018-03-31 09:01:11 -0500

    Scale Right Position
    Written by Katherine Jesse
    
    Co-Authored-By: bugeybot <bugeybot@users.noreply.github.com>
    Co-Authored-By: kcjesse02 <kcjesse02@users.noreply.github.com>

--- src/org/usfirst/frc1073/robot18/commands/AutonomousChooserSimple/Auto3CommandR_LLSimple.java
+++ src/org/usfirst/frc1073/robot18/commands/AutonomousChooserSimple/Auto3CommandR_LLSimple.java
@@ -13,5 +13,12 @@ public class Auto3CommandR_LLSimple extends CommandGroup {
 	/** If Chooser is set to Right and FMS is LLL */
 	public Auto3CommandR_LLSimple() {
 		addSequential(new AdvancedDrive(-.8, 150, 100));
+		addSequential(new TurnWithGyro(-.8, 90,"counterclockwise"));
+		addSequential(new AdvancedDrive(-.8, 150, 0));
+		addSequential(new TurnWithGyro(-.8, 90, "clockwise"));
+		addParallel(new ElevatorMove("high", 1, 85));
+		addSequential(new AdvancedDrive(-.8, 25, 0));
+		addSequential(new TurnWithGyro(-.8, 90, "clockwise"));
+		addSequential(new SpitOutCube(.5));
 	}
 }
