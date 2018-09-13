caa58a752f978512283697a312125281774fbc18
hallettc <iprogram20@gmail.com>
2018-09-12 20:19:42 -0400

    elbowv2

--- src/org/usfirst/frc1073/robot18/commands/AutonomousTools/ElbowFlip.java
+++ src/org/usfirst/frc1073/robot18/commands/AutonomousTools/ElbowFlip.java
@@ -11,29 +11,38 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc1073.robot18.commands.HighGearDT;
 import org.usfirst.frc1073.robot18.commands.LowGearDT;
 import org.usfirst.frc1073.robot18.commands.AutonomousTools.*;
-//
+
 /*** If Chooser is set to Center and FMS is LLL */
 public class ElbowFlip extends Command {
 	/** 
 	 * @author Cam
 	 */
+	double time = 0;
+	boolean finished;
+	
 	protected void initialize() {
 		RobotMap.elbowMotor.setSelectedSensorPosition(0, 0, 0);
 	}
 	protected void execute(){
+		while(time < 1) {
+			Robot.elevator.elevatorDrive.tankDrive(-.5, -.5);
+			time = time + 1;
+			if(time == 1) {
+				break;
+			}
+		}
 		if(RobotMap.elbowMotor.getSelectedSensorPosition(0)<=1000 || RobotMap.elbowMotor.getSelectedSensorPosition(0 )>=-1000) {
-			Robot.elevator.elbowDrive.tankDrive(1, 1);
+			Robot.elevator.elbowDrive.tankDrive(-.5, -.5);
+			finished = false;
 		}
 		else {
 			Robot.elevator.elbowDrive.tankDrive(0, 0);
+			finished = true;
+		//	isFinished = true;
 		}
 	}
 	protected boolean isFinished() {
-		if(RobotMap.elbowMotor.getSelectedSensorPosition(0)<=1000 || RobotMap.elbowMotor.getSelectedSensorPosition(0 )>=-1000) {
-			return false;
-		}
-		else {
-			return true;
-		}
+	return finished;
 	}
+	
 }
